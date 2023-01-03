214b0812c7c9f8611e125bd2317b608faa5bc390
Rodrigo Hernan Saucedo <rodrigohernansaucedo@gmail.com>
2023-01-03 08:04:23 -0500

    comienzo de middleware

---
+++ middlewares/validator.js
@@ -0,0 +1,16 @@
+
+
+const validator= (schema) => [
+    (req,res, next) => {
+        const data= schema.validate(req.body, {aboartEarly:false})
+        if (data.error) {
+            return res.status(400).json({
+                success: false,
+                message: data.error.details
+            })
+        }
+        next()
+    }
+]
+
+export default validator
--- routes/comics.route.js
+++ routes/comics.route.js
@@ -1,12 +1,13 @@
 import controller from "../controllers/comics.controllers.js";
 import express from 'express'
 import schema from "../schema/comics.schema.js";
+import validator from "../middlewares/validator.js";
 
 const router = express.Router() 
 
 
 const { create } = controller
 
-router.post('/', create) 
+router.post('/', validator(schema) ,create)  
 
 export default router  
