90e594f183b03f718e9c280319000a732d9506b5
Lazaro Tomas Del Prado <lazaro.delprado@alu.frt.utn.edu.ar>
2023-01-05 12:23:43 -0300

    schema update

--- schemas/chapter.schema.js
+++ schemas/chapter.schema.js
@@ -3,7 +3,7 @@ import Joi from "joi-oid"
 export const createSchema = Joi.object(
     {
         comic_id: Joi.objectId().required(),
-        title: Joi.string().required().min(1),
+        title: Joi.string().required().min(1).max(200),
         pages: Joi.array().items(Joi.string().uri()).required(),
         order: Joi.number()
     }
