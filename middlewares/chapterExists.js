e0cd733a39aef30ec35d373acbecbbcea879df0b
Lazaro Tomas Del Prado <lazaro.delprado@alu.frt.utn.edu.ar>
2023-01-05 05:53:08 -0300

    middleware order

--- middlewares/chapterExists.js
+++ middlewares/chapterExists.js
@@ -1,26 +1,32 @@
 import { Chapter } from "../models/Chapter.model.js"
 import defaultResponse from '../config/response.js'
 
+
 async function chapterExists(req, res, next) {
     const chapter = await Chapter.findOne({order: req.body.order})
-    const comic = await Chapter.findOne({comic_id: req.body.comic_id})
-    if (chapter && comic) {
-        let newOrder = chapter?.order+1
-        req.chapter = {
-            comic_id: chapter?.comic_id,
-            title: chapter?.title,
-            pages: chapter?.pages,
-            order: newOrder,
+    const lastChapter = (await Chapter.find())
+    if (!chapter) {
+        if(chapter === null){
+            req.chapter = {
+                comic_id: chapter?.comic_id,
+                title: chapter?.title,
+                pages: chapter?.pages,
+            }
+            req.body.order = lastChapter[lastChapter.length-1].order+1
+        }else{
+            req.chapter = {
+                comic_id: chapter?.comic_id,
+                title: chapter?.title,
+                pages: chapter?.pages,
+                order: chapter?.order
+            }
         }
-        console.log(newOrder)
         return next()
     }
-    
     req.body.success = false
     req.body.sc = 400
-    req.body.data = 'order no exist!'
+    req.body.data = 'chapter exist!'
     return defaultResponse(req,res)
-
 }
 
 export default chapterExists
