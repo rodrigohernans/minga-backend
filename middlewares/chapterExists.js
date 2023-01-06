d78e33f6d910f0cc866c686d33c4620e1a7bf57d
Lazaro Tomas Del Prado <lazaro.delprado@alu.frt.utn.edu.ar>
2023-01-06 03:10:24 -0300

    test middleware

--- middlewares/chapterExists.js
+++ middlewares/chapterExists.js
@@ -4,29 +4,20 @@ import defaultResponse from '../config/response.js'
 
 async function chapterExists(req, res, next) {
     const chapter = await Chapter.findOne({order: req.body.order})
-    const lastChapter = (await Chapter.find())
-    if (!chapter) {
-        if(chapter === null){
-            req.chapter = {
-                comic_id: chapter?.comic_id,
-                title: chapter?.title,
-                pages: chapter?.pages,
-            }
-            req.body.order = lastChapter[lastChapter.length-1].order+1
-        }else{
-            req.chapter = {
-                comic_id: chapter?.comic_id,
-                title: chapter?.title,
-                pages: chapter?.pages,
-                order: chapter?.order
-            }
+    if(!chapter){
+        req.chapter = {
+            comic_id: chapter?.comic_id,
+            title: chapter?.title,
+            order: chapter?.order,
+            pages: chapter?.pages
         }
         return next()
     }
-    req.body.success = false
+    req.success = false
     req.body.sc = 400
-    req.body.data = 'chapter exist!'
-    return defaultResponse(req,res)
+    req.body.data = 'Chapter exist!'
+    return defaultResponse(req, res)
+
 }
 
 export default chapterExists
