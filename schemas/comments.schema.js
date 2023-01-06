721ee5aebc6c1a6aef08c47fab9bcd0953fa7228
feldrok <matias@moravalero.cl>
2023-01-06 00:12:46 -0300

    fix joi error messages

--- schemas/comments.schema.js
+++ schemas/comments.schema.js
@@ -2,11 +2,11 @@ import Joi from 'joi-oid'
 
 export const createSchema = Joi.object({
   text: Joi.string().required().min(1).max(200).messages({
-    'any.required': `text is a required field`,
-    'string.empty': `text cannot be an empty field`,
-    'string.min': `text must have a minimum length of {#limit}`,
-    'string.max': `text must have a maximum length of {#limit}`,
-    'string.base': `text must be a type of 'text'`
+    'any.required': `comment is a required field`,
+    'string.empty': `comment cannot be an empty field`,
+    'string.min': `comment must have a minimum length of {#limit}`,
+    'string.max': `comment must have a maximum length of {#limit}`,
+    'string.base': `comment must be a type of 'text'`
   }),
   user_id: Joi.objectId().required(),
   chapter_id: Joi.objectId().required(),
