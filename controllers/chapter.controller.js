2ce21e6235fc2ab442a5ed44759232763c9a34c2
Ariel Gonzalez Ayala <119444661+arielgonzalezayala@users.noreply.github.com>
2023-01-10 10:40:44 -0300

    controlador arreglado (envié controlador erróneo sin querer

--- controllers/chapter.controller.js
+++ controllers/chapter.controller.js
@@ -13,24 +13,42 @@ const controller = {
     }
   },
   get_pages: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      let chapter = await Chapter.findById(id);
-      if (chapter) {
-        res.status(200).json({
-          success: true,
-          response: chapter,
-        });
-      } else {
-        res.status(400).json({
-          success: false,
-          response: "Error obtaining chapters",
-        });
-      }
-    } catch (error) {
-      console.log(error);
+    let consultas = {}
+    let ordenamiento = {
+        title: 1
     }
-  },
-};
+    let paginacion = {
+        page: 1,
+        limit: 10
+    }
+    if (req.query.title) {
+        consultas.title = { $regex: req.query.title.trim(), $options: "i" }
+    }
+    if (req.query.category) {
+        consultas.category = req.query.category
+    }
+    if (req.query.sort) {
+        ordenamiento = req.query.sort
+    }
+    if (req.query.page) {
+        paginacion.page = req.query.page
+    }
+    if (req.query.limit) {
+        paginacion.limit = req.query.limit
+    }
+    try {
+        const chapters = await Chapter.find(consultas)
+            .sort(ordenamiento)
+            .skip((paginacion.page - 1) * paginacion.limit)
+            .limit(paginacion.limit)
+        res.status(200).json({
+            success: true,
+            response: chapters,
+        })
+    } catch (error) {
+        next(error)
+    }
+},
+}
 
-export default controller;
+export default controller
