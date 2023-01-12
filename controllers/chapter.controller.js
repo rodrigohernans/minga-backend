612c4580a3cd3d7f7ecde69a1936202c32efc562
Ariel Gonzalez Ayala <119444661+arielgonzalezayala@users.noreply.github.com>
2023-01-12 15:23:03 -0300

    cambios en rutas

--- controllers/chapter.controller.js
+++ controllers/chapter.controller.js
@@ -13,18 +13,56 @@ const controller = {
     }
   },
   get_pages: async (req, res, next) => {
-    const { _id } = req.params
+    const { _id } = req.params;
     try {
-      const comic = await Chapter.findById(_id)
-      console.log(comic)
+      const comic = await Chapter.findById(_id);
+      console.log(comic);
       res.status(200).json({
         success: true,
-        response: comic
-      })
-    }catch (error) {
-      next(error)
+        response: comic,
+      });
+    } catch (error) {
+      res.status(404).json({
+        success: false,
+        response: "Error Obtaining Chapter",
+      });
     }
-},
-}
+  },
+  get_comic_chapters: async (req, res, next) => {
+    let consultas = {};
+    let ordenamiento = {};
+    let paginacion = {
+      page: 1,
+      limit: 5,
+    };
+    if (req.query.comic_id) {
+      consultas.comic_id = req.query.comic_id;
+    }
+    if (req.query.page) {
+      paginacion.page = req.query.page;
+    }
+    if (req.query.limit) {
+      paginacion.limit = req.query.limit;
+    }
+    if (req.query.sort) {
+      ordenamiento = { order: req.query.sort };
+    }
+    try {
+      const chapters = await Chapter.find(consultas)
+        .sort(ordenamiento)
+        .skip(
+          paginacion.page > 0 ? (paginacion.page - 1) * paginacion.limit : 0
+        )
+        .limit(paginacion.limit);
+      res.status(200).json({
+        success: true,
+        response: chapters,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  },
+};
 
-export default controller
+export default controller;
