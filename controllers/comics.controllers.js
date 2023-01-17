116435055d6296a23b21f2eff4afc0c633ce6a4c
Lazaro Tomas Del Prado <lazaro.delprado@alu.frt.utn.edu.ar>
2023-01-17 02:23:35 -0300

    modifica controlador para paginado

--- controllers/comics.controllers.js
+++ controllers/comics.controllers.js
@@ -16,38 +16,38 @@ const controller = {
         let filterByProfile = {}
         let orderByDate = {}
         let pagination = {
-            page: 1
+            limit: 5
         }
-        if(req.query.company_id){
+        if (req.query.company_id) {
             filterByProfile.company_id = req.query.company_id
         }
-        if(req.query.category_id){
+        if (req.query.category_id) {
             filterByProfile.category_id = req.query.category_id
         }
-        if (req.query.page) {
-            pagination.page = req.query.page
+        if (req.query.limit) {
+            pagination.limit = req.query.limit
         }
-        if(orderByDate){
-            orderByDate = {createdAt: "1"}
+        if (orderByDate) {
+            orderByDate = { createdAt: "1" }
         }
         try {
-            let comics = await Comic.find(filterByProfile, "-_id -author_id -company_id -category_id -createdAt -updatedAt -__v")
-            .sort(orderByDate)
-            .skip( pagination.page > 0 ? ((pagination.page - 1) * 5 ) : 0)
-            .limit(5)
-            if(comics.length === 0){
+            let comics = await Comic.find(filterByProfile, "-author_id -company_id -createdAt -updatedAt -__v")
+                .sort(orderByDate)
+                // .skip( pagination.limit > 0 ? ((pagination.limit - 1) * 5 ) : 0)
+                .limit(pagination.limit)
+            if (comics.length === 0) {
                 res.status(404).json({
                     success: false,
                     message: "Comics not found"
                 })
-            } else{
+            } else {
                 res.status(200).json({
                     success: true,
                     response: comics,
                     message: "Comics found"
                 })
             }
-        } catch(error) {
+        } catch (error) {
             next(error)
         }
     }
