7ab3e8c07316a99b57669eea67a11e633e4d1bb9
Rodrigo Hernan Saucedo <rodrigohernansaucedo@gmail.com>
2023-01-25 15:16:11 -0500

    simple commit

--- middlewares/isAuthorAndCompany.js
+++ middlewares/isAuthorAndCompany.js
@@ -4,7 +4,7 @@ async function isAuthorAndCompany(req,res,next) {
 
     if (req.user.is_author || req.user.is_company) {
         return next()
-    }
+    } 
     req.body.success = false
     req.body.sc = 401
     req.body.data = 'you are not allowed'
--- middlewares/verifyAuthor.js
+++ middlewares/verifyAuthor.js
@@ -15,7 +15,9 @@ export const verifyAuthor = async (req, res, next) => {
                 path: "author_id",
                 model: "authors"
             }
-        })
+        }) 
+
+
 
     const { user_id } = chapter.comic_id.author_id
     console.log(user_id)
@@ -28,4 +30,5 @@ export const verifyAuthor = async (req, res, next) => {
         "You must to be the author of the comic to be able to modify or delete";
     return defaultResponse(req, res);
 }
+
 
